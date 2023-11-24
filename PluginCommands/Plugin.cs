4b57ba3286057ad93ce654c0ea913c9d14feb69f
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2023-11-24 19:34:12 +0100

    Updated dependencies

--- PluginCommands/Plugin.cs
+++ PluginCommands/Plugin.cs
@@ -9,8 +9,19 @@ namespace PluginCommands;
 /// </summary>
 public class Plugin
 {
+    /// <summary>
+    /// Contains current plugin version.
+    /// </summary>
     public const string PluginVersion = "3.0.0";
+
+    /// <summary>
+    /// Contains plugin description.
+    /// </summary>
     public const string PluginDescription = "Provides commands for NwPluginAPI based plugins management at runtime.";
+
+    /// <summary>
+    /// Contains plugin author.
+    /// </summary>
     public const string PluginAuthor = "Adam Szerszenowicz";
 
     /// <summary>
