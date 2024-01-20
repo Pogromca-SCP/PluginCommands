27f410af63fd0c617959cc92aecbe02f1b698d4e
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2024-01-20 21:07:41 +0100

    Updated copyright year

--- PluginCommands/Properties/AssemblyInfo.cs
+++ PluginCommands/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyTitle("PluginCommands")]
 [assembly: AssemblyDescription(Plugin.PluginDescription)]
 [assembly: AssemblyProduct("PluginCommands")]
-[assembly: AssemblyCopyright("Copyright © 2023 " + Plugin.PluginAuthor)]
+[assembly: AssemblyCopyright("Copyright © 2024 " + Plugin.PluginAuthor)]
 [assembly: AssemblyVersion(Plugin.PluginVersion)]
 [assembly: AssemblyFileVersion(Plugin.PluginVersion)]
