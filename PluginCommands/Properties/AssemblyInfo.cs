d941a115dd8aec0c52ea5d8bc7acc65b3ce68e34
Adam Szerszenowicz <70147389+Pogromca-SCP@users.noreply.github.com>
2024-09-11 18:50:15 +0200

    Removed update year workflow

--- PluginCommands/Properties/AssemblyInfo.cs
+++ PluginCommands/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("PluginCommands")]
 [assembly: AssemblyDescription(Plugin.PluginDescription)]
 [assembly: AssemblyProduct("PluginCommands")]
-[assembly: AssemblyCopyright("Copyright © 2023-2024 " + Plugin.PluginAuthor)]
+[assembly: AssemblyCopyright("Copyright © 2023-present " + Plugin.PluginAuthor)]
 [assembly: AssemblyVersion(Plugin.PluginVersion)]
 [assembly: AssemblyFileVersion(Plugin.PluginVersion)]
 [assembly: AssemblyInformationalVersion(Plugin.PluginVersion)]
